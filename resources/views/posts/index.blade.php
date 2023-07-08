72a09b7b929cc02bb154fa9f038c27bd47120092
Said <46088969+webmastersaid@users.noreply.github.com>
2023-07-08 22:06:52 +0500

    add pagination of posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,11 +1,13 @@
 @extends('templates.main')
 @section('content')
-    <h1>Posts</h1>
-    <a class="btn btn-primary mb-3" href="{{ route('posts.create') }}">Add new post</a>
-    <ul class="list-group">
-        @foreach ($posts as $key => $post)
-            <li class="list-group-item">{{ $key + 1 }}. <a
-                    href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a></li>
-        @endforeach
-    </ul>
+<h1>Posts</h1>
+<a class="btn btn-primary mb-3" href="{{ route('posts.create') }}">Add new post</a>
+<ul class="list-group mb-3">
+    @foreach ($posts as $key => $post)
+    <li class="list-group-item">{{ $key + 1 }}. <a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a></li>
+    @endforeach
+</ul>
+<div class="mb-3">
+    {{ $posts->links() }}
+</div>
 @endsection
