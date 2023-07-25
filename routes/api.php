432343b03de0d293557ab9f25b97d853909aaeb1
Said <46088969+webmastersaid@users.noreply.github.com>
2023-07-25 16:23:57 +0500

    add jwt token

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['namespace' => 'App\Http\Controllers', 'middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
+Route::group(['namespace' => 'App\Http\Controllers\Post', 'middleware' => 'jwt.auth',], function () {
+    Route::get('/posts', 'IndexController');
+    Route::get('/posts/create', 'CreateController');
+    Route::post('/posts', 'StoreController');
+    Route::get('/posts/{post}', 'ShowController');
+    Route::get('/posts/{post}/edit', 'EditController');
+    Route::patch('/posts/{post}', 'UpdateController');
+    Route::delete('/posts/{post}', 'DestroyController');
+});
