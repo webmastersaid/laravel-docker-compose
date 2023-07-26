41da5c1bf1c84982dbea217baccc6c67dc9ce505
Said <46088969+webmastersaid@users.noreply.github.com>
2023-07-26 22:30:37 +0500

    update post store controller

--- app/Http/Controllers/Post/StoreController.php
+++ app/Http/Controllers/Post/StoreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Post;
 
 use App\Http\Requests\Post\StoreRequest;
 use App\Http\Resources\Post\PostResource;
+use App\Models\Post;
 
 class StoreController extends BaseController
 {
@@ -11,7 +12,7 @@ class StoreController extends BaseController
     {
         $data = $request->validated();
         $post = $this->service->store($data);
-        return new PostResource($post);
+        return $post instanceof Post ? new PostResource($post) : $post;
         // return redirect()->route('post.index');
     }
 }
